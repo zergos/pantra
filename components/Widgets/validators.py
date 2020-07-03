da9fb267fb4bb9e8893687cf85d6931f2f710c19
Zergos <invent@zergos.ru>
2020-07-04 01:02:48 +0300

    widgets initial

---
+++ components/Widgets/validators.py
@@ -0,0 +1,15 @@
+from core.ctx import *
+
+validators = []
+
+
+def validate(func, message):
+    validators.append((func, message))
+
+
+def check_validators():
+    for f, m in validators:
+        if not f():
+            ctx['error'] = m
+            return False
+    return True
