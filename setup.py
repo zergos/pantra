ae6adaf19f9869c396e6f8684be1a2217abf5399
Andrey Aseev <invent@zergos.ru>
2023-11-21 18:24:08 +0800

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pantra',
+    version='1.0',
+    packages=['pantra'],
+    url='https://github.com/zergos/pantra',
+    license='Apache 2.0',
+    author='Andrey Aseev',
+    author_email='invent@zergos.ru',
+    description='Python Full-stack Framework'
+)
