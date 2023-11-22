eb955bab09aadb71bcbb4909d8f087604a057b36
Andrey Aseev <invent@zergos.ru>
2023-11-22 13:41:23 +0800

    fix setup

--- pantra.py
+++ pantra.py
@@ -1,3 +1,9 @@
 # run management command
 from pantra.management import execute_from_command_line
-execute_from_command_line(None)
+
+def main():
+    execute_from_command_line(None)
+
+if __name__ == '__main__':
+    main()
+
