738155a91733c1a4b16dad46eef867595a017687
Andrey Aseev <invent@zergos.ru>
2026-01-20 13:17:42 +0700

    python 3.12+ support

--- pantra/patching.py
+++ pantra/patching.py
@@ -31,16 +31,6 @@ def wipe_logger(func: FT) -> FT:
     if config.ENABLE_LOGGING:
         return func
 
-    """
-    if inspect.isclass(func):
-        for k, v in func.__dict__.items():
-            if inspect.isfunction(v):
-                setattr(func, k, wipe_logger(v))
-            elif type(v) is staticmethod:
-                setattr(func, k, staticmethod(wipe_logger(v.__func__)))
-        return func
-    """
-
     source_lines, line_num = inspect.getsourcelines(func)
     source = '\n' * (line_num-1) + textwrap.dedent(''.join(source_lines))
     tree = ast.parse(source)
@@ -59,64 +49,3 @@ def wipe_logger(func: FT) -> FT:
     exec(code_obj, namespace)
     return namespace[func_name]
 
-"""
-def wipe_logger(func: FT) -> FT:
-    from .settings import config
-    if config.ENABLE_LOGGING:
-        return func
-
-    if inspect.isclass(func):
-        for k, v in func.__dict__.items():
-            if inspect.isfunction(v):
-                setattr(func, k, wipe_logger(v))
-            elif type(v) is staticmethod:
-                setattr(func, k, staticmethod(wipe_logger(v.__func__)))
-        return func
-
-    load_global = dis.opmap['LOAD_GLOBAL']
-    nop = dis.opmap['NOP']
-    call_method = dis.opmap['CALL']
-
-    bytecode = list(func.__code__.co_code)
-    mode = 0
-    for n, byte in enumerate(bytecode[::2]):
-        op = byte
-        i = n * 2
-        arg = bytecode[i + 1]
-        if mode == 0:
-            if op == load_global and func.__code__.co_names[arg>>1] == "logger":
-                bytecode[i] = nop
-                mode = 1
-        elif mode == 1:
-            bytecode[i] = nop
-            if op == call_method:
-                bytecode[i + 2] = nop
-                mode = 0
-
-    return types.FunctionType(
-        types.CodeType(
-            func.__code__.co_argcount,
-            func.__code__.co_posonlyargcount,
-            func.__code__.co_kwonlyargcount,
-            func.__code__.co_nlocals,
-            func.__code__.co_stacksize,
-            func.__code__.co_flags,
-            bytes(bytecode),
-            func.__code__.co_consts,
-            func.__code__.co_names,
-            func.__code__.co_varnames,
-            func.__code__.co_filename,
-            func.__code__.co_name,
-            func.__code__.co_qualname,
-            func.__code__.co_firstlineno,
-            func.__code__.co_linetable,
-            func.__code__.co_exceptiontable,
-            func.__code__.co_freevars,
-            func.__code__.co_cellvars,
-        ),
-        func.__globals__,
-        func.__name__,
-        func.__defaults__,
-        func.__closure__,
-    )
-"""
